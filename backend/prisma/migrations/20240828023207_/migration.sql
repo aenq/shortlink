0439c6bd5b9613a3e246a1eef8d0c370b8f46c66
rachel83 <rachel@xurya.com>
2024-10-10 22:42:06 +0700

    init fe

--- backend/prisma/migrations/20240828023207_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `shortUrl` on table `Shortlinks` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Shortlinks" ALTER COLUMN "shortUrl" SET NOT NULL;
